e3f3aa71f4914284616fa6cf57e7a3bef83661f8
raja mehmood <rajamehmood224@gmail.com>
2024-04-05 21:34:45 +0400

    final

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 const apiKey = "97243de2873d401d8203fd64d476c025"; // Replace with your actual API key
 export const apiUrl = `https://newsapi.org/v2/top-headlines?x=y&country=us&Political%20News=GENERAL&pageSize=50&apiKey=${apiKey}`;
 
-export const Api = "http://localhost:3004";
-//export const Api = //'https://phpstack-1080450-4052700.cloudwaysapps.com'//
+//export const Api = "http://localhost:3004";
+export const Api = 'https://phpstack-1080450-4052700.cloudwaysapps.com'//
